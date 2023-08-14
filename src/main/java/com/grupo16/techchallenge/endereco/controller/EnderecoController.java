102cd75636a3b08a5a3c37572f2c2b07d8cece7f
tatiane <tati.guisso@gmail.com>
2023-08-14 19:51:51 -0300

    clean code

--- src/main/java/com/grupo16/techchallenge/endereco/controller/EnderecoController.java
+++ src/main/java/com/grupo16/techchallenge/endereco/controller/EnderecoController.java
@@ -11,14 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grupo16.techchallenge.endereco.controller.json.EnderecoJson;
 import com.grupo16.techchallenge.endereco.domain.Endereco;
-import com.grupo16.techchallenge.endereco.usecase.ObterEnderecoUseCase;
 import com.grupo16.techchallenge.endereco.usecase.CriarAlterarEnderecoUseCase;
+import com.grupo16.techchallenge.endereco.usecase.ObterEnderecoUseCase;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/com/grupo16/techchallenge/endereco/gateway/repository/mysql/EnderecoMySQLGateway.java
+++ src/main/java/com/grupo16/techchallenge/endereco/gateway/repository/mysql/EnderecoMySQLGateway.java
@@ -1,6 +1,5 @@
 package com.grupo16.techchallenge.endereco.gateway.repository.mysql;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
