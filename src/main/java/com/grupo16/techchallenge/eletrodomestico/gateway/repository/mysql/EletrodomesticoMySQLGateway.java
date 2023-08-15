2ae6ecfecff5fe472df966b1d76292b3b1ee751a
bruno-gds <brunog.damascena@gmail.com>
2023-08-15 15:53:31 -0300

    feat: implementando EletrodomesticoMySQLGateway

---
+++ src/main/java/com/grupo16/techchallenge/eletrodomestico/gateway/repository/mysql/EletrodomesticoMySQLGateway.java
@@ -0,0 +1,18 @@
+package com.grupo16.techchallenge.eletrodomestico.gateway.repository.mysql;
+
+import com.grupo16.techchallenge.eletrodomestico.domain.Eletrodomestico;
+import com.grupo16.techchallenge.eletrodomestico.gateway.EletrodomesticoRepositoryGateway;
+
+/**
+ * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
+ * Date: 15/08/2023
+ * Project Name: tech-challenge
+ */
+
+public class EletrodomesticoMySQLGateway implements EletrodomesticoRepositoryGateway {
+
+    @Override
+    public Long criar(Eletrodomestico eletrodomestico) {
+        return null;
+    }
+}
