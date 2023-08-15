f66fe1a99f664a3e2cfb64a160c6bb4dca3c03fe
bruno-gds <brunog.damascena@gmail.com>
2023-08-15 20:32:44 -0300

    refact: implementando criar Eletrodomestico

--- src/main/java/com/grupo16/techchallenge/eletrodomestico/gateway/repository/mysql/EletrodomesticoMySQLGateway.java
+++ src/main/java/com/grupo16/techchallenge/eletrodomestico/gateway/repository/mysql/EletrodomesticoMySQLGateway.java
@@ -1,5 +1,9 @@
 package com.grupo16.techchallenge.eletrodomestico.gateway.repository.mysql;
 
+import com.grupo16.techchallenge.eletrodomestico.gateway.exception.ErrorToAccessDatabaseException;
+import com.grupo16.techchallenge.eletrodomestico.gateway.repository.jpa.repository.EletrodomesticoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.grupo16.techchallenge.eletrodomestico.domain.Eletrodomestico;
@@ -11,11 +15,26 @@ import com.grupo16.techchallenge.eletrodomestico.gateway.EletrodomesticoReposito
  * Project Name: tech-challenge
  */
 
+@Slf4j
 @Repository
 public class EletrodomesticoMySQLGateway implements EletrodomesticoRepositoryGateway {
 
+    @Autowired
+    private EletrodomesticoRepository eletrodomesticoRepository;
+
     @Override
     public Long criar(Eletrodomestico eletrodomestico) {
-        return null;
+        try {
+            log.trace("Start eletrodomestico={}", eletrodomestico);
+
+            eletrodomesticoRepository.save(eletrodomestico);
+
+            log.trace("End id={}", eletrodomestico.getId());
+            return eletrodomestico.getId();
+
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new ErrorToAccessDatabaseException();
+        }
     }
 }
