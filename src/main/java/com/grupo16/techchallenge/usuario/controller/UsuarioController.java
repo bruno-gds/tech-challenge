583618d516baa4a0c33b2baabc07896e0710d2bd
tatiane <tati.guisso@gmail.com>
2023-08-20 11:22:47 -0300

    feat: iniciando endpoints do UsuarioController

--- src/main/java/com/grupo16/techchallenge/usuario/controller/UsuarioController.java
+++ src/main/java/com/grupo16/techchallenge/usuario/controller/UsuarioController.java
@@ -1,10 +1,18 @@
 package com.grupo16.techchallenge.usuario.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/usuarios")
 @RestController
 public class UsuarioController {
-	
+
 	@Autowired
 	private CriarAlterarUsuarioUseCase userUseCase;
 
@@ -29,12 +37,61 @@ public class UsuarioController {
 			@Valid 
 			@RequestBody UsuarioJson usuarioJson) {
 		log.trace("Start usuarioJson={}", usuarioJson);
-		
+
 		Usuario usuario = usuarioJson.mapearParaUsuarioDomain();
-		
+
 		Long usuarioId = userUseCase.criar(usuario);
 
 		log.trace("End usuarioId={}", usuarioId);
 		return usuarioId;
+	}
+
+	@GetMapping("{cpf}")
+	public UsuarioJson obter(
+			@PathVariable(name = "cpf", required = true) Long cpf) {
+		log.trace("Start cpf={}", cpf);
+
+		//TODO: implementar
+		
+		log.trace("End usuario={}");
+		return null;
+	}
+	
+	@PatchMapping
+	public void alterar(
+			@RequestBody(required = true) UsuarioJson usuarioJson) {
+		log.trace("Start usuarioJson={}", usuarioJson);
+		
+		//TODO: implementar
+		
+		log.trace("End");
+		
+	}
+	
+	@DeleteMapping("{id}")
+	public void remover(
+			@PathVariable(name = "id", required = true) Long id) {
+		log.trace("Start id={}", id);
+
+		//TODO: implementar
+		
+		log.trace("End usuario={}");		
+	}
+	
+	@GetMapping
+	public List<UsuarioJson> buscar(
+			@RequestParam(name = "nome", required = false) String nome,
+			@RequestParam(name = "cpf", required = false) String cpf,
+			@RequestParam(name = "parentesco", required = false) String parentesco,
+			@RequestParam(name = "dataNascimento", required = false) LocalDate dataNascimento,
+			@RequestParam(name = "genero", required = false) String genero){
+		
+		/*
+		 * TODO implementar
+		 * A busca deve ser capaz de filtrar as informações por nome, parentesco, 
+		 * sexo ou outra informação relevante.
+		 */
+		
+		return null;
 	}
 }
