3bad151e8838834f9c1887d4130166e62b09c65e
tatiane <tati.guisso@gmail.com>
2023-08-22 19:18:25 -0300

    fix: alteracao de Patch para Put

--- src/main/java/com/grupo16/techchallenge/endereco/controller/EnderecoController.java
+++ src/main/java/com/grupo16/techchallenge/endereco/controller/EnderecoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,7 +67,7 @@ public class EnderecoController {
 		
 	}
 	
-	@PatchMapping
+	@PutMapping
 	public void alterar(
 			@RequestBody(required = true) EnderecoJson enderecoJson) {
 		log.trace("Start enderecoJson={}", enderecoJson);
--- src/main/java/com/grupo16/techchallenge/usuario/controller/UsuarioController.java
+++ src/main/java/com/grupo16/techchallenge/usuario/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -64,7 +65,7 @@ public class UsuarioController {
 		return usuarioJson;
 	}
 	
-	@PatchMapping
+	@PutMapping
 	public void alterar(
 			@RequestBody(required = true) UsuarioJson usuarioJson) {
 		log.trace("Start usuarioJson={}", usuarioJson);
